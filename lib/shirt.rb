1e130e60bd1e6e17f2ba9ed31e1782bfc1a06bf5
aracelicaes <aracelicaes@gmail.com>
2020-06-29 18:26:10 -0500

    Created a Module called PRINTER and moved print_all method there Created test cases for Shirt class also the Printer Module

--- lib/shirt.rb
+++ lib/shirt.rb
@@ -7,8 +7,8 @@ class Shirt
 
   def initialize(raw_info)
     @item_name = raw_info.children[3].children[1].children[0].text
-    @item_price = raw_info.children[3].children[3].children[1].children[1]
-    @item_url = raw_info.children[3].children[1].children[0].attributes['href']
+    @item_price = raw_info.children[3].children[3].children[1].children[1].text
+    @item_url = raw_info.children[3].children[1].children[0].attributes['href'].to_s
     name_caps
   end
 
